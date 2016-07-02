36b0a14282a0b0ca30a2637f45bc379b03357869
godric <godric.com@gmail.com>
2016-07-02 16:51:13 +0200

    add: PoC for python slackbot

---
+++ python_slack/slackbot_settings.py
@@ -0,0 +1,10 @@
+
+API_TOKEN = "xoxb-56402013345-kjEXZAxunHuLwcXckS57gk4F"
+
+DEFAULT_REPLY = "Sorry but this does not look like a soccer to me."
+
+PLUGINS = [
+  'slackbot.plugins',
+  'responder',
+]
+
